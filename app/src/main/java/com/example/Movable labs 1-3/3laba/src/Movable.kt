6c804c5fb8db2eab8b9220722cae1aee6e65b372
Nikita <nikita71982006@mail.ru>
2025-12-24 17:08:35 +0700

    add media player and location

--- app/src/main/java/com/example/Movable labs 1-3/3laba/src/Movable.kt
+++
@@ -1,6 +0,0 @@
-interface Movable{
-    var speed: Int
-    var x: Int
-    var y: Int
-    fun moveTo()
-}
--- app/src/main/java/com/example/Movable labs 1-3/3laba/src/driver.kt
+++
@@ -1,8 +0,0 @@
-class Driver(_fio: String, _age: Int, _sp: Int, _dr: Boolean) : Human(_fio, _age, _sp, _dr){
-
-    override fun moveTo(){
-        x += speed
-        println("${FIO} is moved to $x")
-    }
-
-}
